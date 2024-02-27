0653152e1a6c546bf5f2070969f7f865d6ec3b17
MCavieres <maca.c.91@gmail.com>
2024-02-27 17:17:17 -0300

    implemente la logica del resultado

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,37 +5,68 @@ const btnVeces = document.querySelector("#btnVeces");
 
 const btnEleccion = document.querySelector("#btnEleccion");
 
-const elecciones = document.querySelector("#elecciones");
-const p = document.createElement("p");
-elecciones.appendChild(p);
-
 const user = document.querySelector("#user");
-const span = document.createElement("span");
-user.appendChild(span);
+const parra = document.createElement("p");
+user.appendChild(parra);
 
-let eleccionComputador = Math.floor(Math.random() * 3);
+const computador = document.querySelector("#computador");
+const p = document.createElement("p");
+computador.appendChild(p);
 
-switch (eleccionComputador) {
-    case 0:
-        eleccionComputador = "piedra";
-        break;
-    case 1:
-        eleccionComputador = "papel";
-        break;
-    case 2:
-        eleccionComputador = "tijera";
-        break;
-}
-console.log(eleccionComputador);
+const resultado = document.querySelector("#resultado");
+const parr = document.createElement("p");
+resultado.appendChild(parr);
 
 btnEleccion.addEventListener("click", function () {
     let eleccion = document.querySelector("#eleccion").value;
-    if (eleccion === "1") {
-        console.log("Elegiste piedra");
-    } else if (eleccion === "2") {
-        console.log("Elegiste papel");
+    let eleccionNum = +eleccion;
+
+    if (eleccionNum === 0) {
+        user.innerText = "Elegiste Piedra";
+        eleccionNum = "Piedra";
+    } else if (eleccionNum === 1) {
+        user.innerText = "Elegiste Papel";
+        eleccionNum = "Papel";
     } else {
-        console.log("Elegiste tijera");
+        user.innerText = "Elegiste Tijera";
+        eleccionNum = "Tijera";
+    }
+
+    console.log(eleccionNum);
+
+    let eleccionComputador = Math.floor(Math.random() * 3);
+
+    switch (eleccionComputador) {
+        case 0:
+            eleccionComputador = "Piedra";
+            break;
+        case 1:
+            eleccionComputador = "Papel";
+            break;
+        case 2:
+            eleccionComputador = "Tijera";
+            break;
+    }
+    computador.innerText = `Computador eligio ${eleccionComputador}`;
+
+    console.log(eleccionComputador);
+
+    if (eleccionComputador === eleccionNum) {
+        parr.innerText = "Empate";
+    } else if (eleccionComputador === "Piedra" && eleccionNum === "Tijera") {
+        parr.innerText = "Gana computador";
+    } else if (eleccionComputador === "Papel" && eleccionNum === "Tijera") {
+        parr.innerText = "Ganaste";
+    } else if (eleccionComputador === "Piedra" && eleccionNum === "Papel") {
+        parr.innerText = "Ganaste";
+    } else if (eleccionComputador === "Tijera" && eleccionNum === "Papel") {
+        parr.innerText = "Gana computador";
+    } else if (eleccionComputador === "Papel" && eleccionNum === "Piedra") {
+        parr.innerText = "Gana computador";
+    } else if (eleccionComputador === "Tijera" && eleccionNum === "Piedra") {
+        parr.innerText = "Ganaste";
+    } else {
+        console.log("error");
     }
 });
 
